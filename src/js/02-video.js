1b229c9e6e62fe4237bbb4032a48835f41f04e31
Valerii Arlamovskyi <valera.arl96@gmail.com>
2023-05-07 18:03:34 +0200

    Finish 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCAL_KEY = "videoplayer-current-time";
+const onPlay = function(data) {
+
+    const { duration, percent, seconds } = data;
+    localStorage.setItem(LOCAL_KEY, seconds);
+};
+player.on('timeupdate', throttle(onPlay, 1000))
+
+const GET_LOCAL_KEY = localStorage.getItem(LOCAL_KEY);
+    player.setCurrentTime(GET_LOCAL_KEY).then(function(seconds) {
+    
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                break;
+            default:
+                // some other error occurred
+                break;
+        }
+    });
+    
