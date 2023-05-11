84d58ae2f815c89a856c5fc7f64bcb5cc39628be
Valerii Arlamovskyi <valera.arl96@gmail.com>
2023-05-11 18:00:05 +0200

    dsadsa

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,40 +1,37 @@
 const refs = {
-    form:document.querySelector('.feedback-form'),
-    input:document.querySelector('.feedback-form input'),
-    textarea:document.querySelector('.feedback-form textarea'),
-
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
 };
 
-refs.form.addEventListener( 'submit', onFormInput );
+refs.form.addEventListener('submit', onForm);
 refs.input.addEventListener('input', onInput);
 refs.textarea.addEventListener('input', onTextareaInput);
 
-populateTextarea();
+// populateTextarea();
 
-const KEY_FORM = "feedback-form-state";
+const KEY_FORM = 'feedback-form-state';
 function onForm(ev) {
-    ev.preventDefault();
-    ev.currentTarget.reset();
-    localStorage.removeItem(KEY_FORM);
+  ev.preventDefault();
+  ev.currentTarget.reset();
+  localStorage.removeItem(KEY_FORM);
 }
 
-function onInput(ev){
-    const inputEl = ev.target.value;
-    localStorage.setItem(KEY_FORM, inputEl)
+function onInput(ev) {
+  const inputEl = ev.target.value;
+  localStorage.setItem(KEY_FORM, inputEl);
 }
-
 
 function onTextareaInput(ev) {
-    const msg = ev.target.value;
-    
-    localStorage.setItem(KEY_FORM, msg)
-}
-function populateTextarea(ev){
-    const saveMsg = localStorage.getItem(KEY_FORM);
-    if(saveMsg){
-        console.log(saveMsg);
-    
-    refs.textarea.value = saveMsg;
-}
-}
+  const msg = ev.target.value;
 
+  localStorage.setItem(KEY_FORM, msg);
+}
+function populateTextarea(ev) {
+  const saveMsg = localStorage.getItem(KEY_FORM);
+  if (saveMsg) {
+    console.log(saveMsg);
+
+    refs.textarea.value = saveMsg;
+  }
+}
